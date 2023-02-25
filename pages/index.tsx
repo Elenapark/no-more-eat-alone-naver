0ad43932e192636075385f88229f69571acce4b9
Sungeun Park <sungeun33@gmail.com>
2023-02-25 16:32:44 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import SampleComponent from '@/src/components/sample-component';
+
+export default function Home() {
+  return (
+    <div>
+      Hello, NextJS
+      <SampleComponent />
+    </div>
+  );
+}
